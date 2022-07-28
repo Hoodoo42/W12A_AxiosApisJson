01bf08e12050fb587ea2fc4b69d5b0cc95fe2c22
Hoodoo42 <create@hoodooverse.ca>
2022-07-27 22:16:23 -0600

    added comments and formatted the code

--- js/app.js
+++ js/app.js
@@ -1,3 +1,5 @@
+//callAxios function will start a request to the api with the chosen url, and call for either a success or failure function depending on the status
+
 function callAxios(details) {
   axios
     .request({
@@ -7,13 +9,18 @@ function callAxios(details) {
     .catch(failureFunction);
 }
 
+// the success function will display a new random dog image provided by the api everytime the button is clicked
 function successFunction(response) {
-  document.body.insertAdjacentHTML(`beforeend`, `<img src="${response[`data`][`message`]}" alt="">`);
+  document.body.insertAdjacentHTML(
+    `beforeend`,
+    `<img src="${response[`data`][`message`]}" alt="">`
+  );
 }
-
+// the failurefunction will display "error" if there was any issue in getting the api request
 function failureFunction(error) {
   document.body.insertAdjacentHTML(`afterbegin`, `<h3>Error!</h3>`);
 }
 
+// Starting with grabbing the element, here by its id, and adding an eventListener that will listen for the button click and start the callAxios function
 let button = document.getElementById(`button`);
 button.addEventListener(`click`, callAxios);
